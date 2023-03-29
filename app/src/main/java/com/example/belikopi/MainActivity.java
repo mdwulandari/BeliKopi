2dc273b6c4567d317e377c05dad100013b60e13e
Wulan <nimadewulandari9@gmail.com>
2023-03-29 23:38:52 +0800

    sugsyduys

--- app/src/main/java/com/example/belikopi/MainActivity.java
+++ app/src/main/java/com/example/belikopi/MainActivity.java
@@ -46,6 +46,26 @@ public class MainActivity extends AppCompatActivity {
         String name=nameEditText.getText().toString();
         Log.v("MainActivity","Nama:"+name);
 
+        CheckBox kopisusuchekbox= (CheckBox) findViewById(R.id.KopiSusu_checkbox);
+        boolean haskopisusu=kopisusuchekbox.isChecked();//mengidentifikasi check
+        Log.v("MainActivity","has kopisusu:"+haskopisusu);
+
+        CheckBox cappucinochekbox= (CheckBox) findViewById(R.id.Cappucino_checkbox);
+        boolean hascappucino=cappucinochekbox.isChecked();//mengidentifikasi check
+        Log.v("MainActivity","has cappucino:"+hascappucino);
+
+        CheckBox nescafechekbox= (CheckBox) findViewById(R.id.Nescafe_checkbox);
+        boolean hasnescafe=nescafechekbox.isChecked();//mengidentifikasi check
+        Log.v("MainActivity","has nescafe:"+hasnescafe);
+
+        CheckBox creamylattechekbox= (CheckBox) findViewById(R.id.CreamyLatte_checkbox);
+        boolean hascreamylatte=creamylattechekbox.isChecked();//mengidentifikasi check
+        Log.v("MainActivity","has creamylatte:"+hascreamylatte);
+
+        CheckBox espressochekbox= (CheckBox) findViewById(R.id.Espresso_checkbox);
+        boolean hasespresso=espressochekbox.isChecked();//mengidentifikasi check
+        Log.v("MainActivity","has espresso:"+hasespresso);
+
         CheckBox whippedcreamChekBox= (CheckBox) findViewById(R.id.WhippedCream_checkbox);
         boolean haswhippedcream=whippedcreamChekBox.isChecked();//mengidentifikasi check
         Log.v("MainActivity","has whippedcream:"+haswhippedcream);
@@ -58,13 +78,33 @@ public class MainActivity extends AppCompatActivity {
         boolean haswaffles=wafflesChekBox.isChecked();//mengidentifikasi check
         Log.v("MainActivity","has waffles:"+haswaffles);
 
-        int price=calculateprice(haswhippedcream,haschocolate,haswaffles);//memanggil method jumlah harga
-        String pricemessage=createOrderSummary(price,name,haswhippedcream,haschocolate,haswaffles);
+        int price=calculateprice(haskopisusu, hascappucino, hasnescafe, hascreamylatte, hasespresso, haswhippedcream,haschocolate,haswaffles);//memanggil method jumlah harga
+        String pricemessage=createOrderSummary(price,name,haskopisusu, hascappucino, hasnescafe, hascreamylatte, hasespresso,haswhippedcream,haschocolate,haswaffles);
 
         displayMessage(pricemessage);
     }
-    private int calculateprice(boolean addwhipedcream,boolean addchocolate,boolean addwaffles){//jumlah pesanan * harga
-        int harga=5000;
+    private int calculateprice(boolean addkopisusu, boolean addcappucino, boolean addnescafe, boolean addcreamylatte, boolean addespresso, boolean addwhipedcream,boolean addchocolate,boolean addwaffles){//jumlah pesanan * harga
+        int harga=0;
+
+        if(addkopisusu){
+            harga=5000;
+        }
+
+        if(addcappucino){
+            harga=10000;
+        }
+
+        if(addnescafe){
+            harga=15000;
+        }
+
+        if(addcreamylatte){
+            harga=20000;
+        }
+
+        if(addespresso){
+            harga=25000;
+        }
 
         if(addwhipedcream){
             harga=harga+1000;//harga tambahan toping
@@ -79,8 +119,13 @@ public class MainActivity extends AppCompatActivity {
         }
         return quantity * harga;
     }
-    private String createOrderSummary(int price, String name, boolean addChocolate, boolean addWhippedCream, boolean addWaffles) {//hasil pemesanan
+    private String createOrderSummary(int price, String name, boolean addKopisusu, boolean addCappucino, boolean addNescafe, boolean addCreamylatte, boolean addEspresso, boolean addChocolate, boolean addWhippedCream, boolean addWaffles) {//hasil pemesanan
         String pricemessage=" Name : "+name;
+        pricemessage+="\n Add Kopi Susu     : " +addKopisusu;
+        pricemessage+="\n Add Cappucino     : " +addCappucino;
+        pricemessage+="\n Add Nescafe       : " +addNescafe;
+        pricemessage+="\n Add Creamy Latte  : " +addCreamylatte;
+        pricemessage+="\n Add Espresso      : " +addEspresso;
         pricemessage+="\n Add Cream         : " +addWhippedCream;
         pricemessage+="\n Add Chocolatos : " +addChocolate;
         pricemessage+="\n Add Waffle     : " +addWaffles;
